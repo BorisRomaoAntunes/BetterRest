fc5788744eeabe8dec173f4c8d23c6fa83e703bf
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-05-03 20:37:24 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Boris R on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
